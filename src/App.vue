20d15cfa3c2fb3dee086676bdde7153ae9232aba
Saugat Gurung <fangurung90@gmail.com>
2026-03-07 17:00:44 +0545

    v-on:method

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <script setup>
-
+import MyEvent from './Components/MyEvent.vue'
 
 </script>
 
 
 <template>
      <h1>Hello World</h1>
+     <MyEvent />
 </template>
 
 
---
+++ src/Components/MyEvent.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { ref } from "vue";
+let count = ref(0);
+
+const incrementCounter = () => count.value++;
+const decrementCounter = () => count.value--;
+
+</script>
+
+<!-- Reactivity: When data are updated the browser updates automatically: reactive() and ref() -->
+<template>
+  <p>{{ count }}</p>
+  <button v-on:click="incrementCounter">Increment</button>
+  <button v-on:click="decrementCounter">Decrement</button> 
+</template>
